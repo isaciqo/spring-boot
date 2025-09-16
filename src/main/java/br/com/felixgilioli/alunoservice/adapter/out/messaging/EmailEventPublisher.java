425dfa2c387f6948b31797e078ea270668a405e9
isaciqo <isaciqo.42@gmail.com.com>
2025-09-15 22:35:08 -0300

    Feat: ADD rabbit

---
+++ src/main/java/br/com/felixgilioli/alunoservice/adapter/out/messaging/EmailEventPublisher.java
@@ -0,0 +1,2 @@
+package br.com.felixgilioli.alunoservice.adapter.out.messaging;public class EmailEventPublisher {
+}
---
+++ src/main/java/br/com/felixgilioli/alunoservice/service/EmailService.java
@@ -0,0 +1,2 @@
+package br.com.felixgilioli.alunoservice.service;public class EmailService {
+}
